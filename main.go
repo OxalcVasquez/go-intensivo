78e58d4ffc2dacde582800478bbb211641002403
oxalcvasquez <oxalc0101@gmail.com>
2025-03-30 17:41:35 -0500

    adedd: punteros

--- main.go
+++ main.go
@@ -1,71 +1,28 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-// funcion classic
-func suma(a, b int) int {
-	return a + b
+var a = 1
+
+// referencia de nombre "a" que apunta a un espacio de memoria que contiene en su interior el valor 1
+
+func incrementar(numero *int) {
+	//NUNCA modifiques el argumento directamente
+	*numero++
 }
 
 // funcion que devuelve mas de un valor
-func dividir(a, b float64) (float64, error) {
-	if b == 0 {
-		fmt.Errorf("No se puede devidir por 0")
-		return 0, errors.New("No se puede divir por cero")
-	}
-	cociente := a / b
-	return cociente, nil
-}
-
-// función con número variable de arguments
-func imprimirNombres(nombres ...string) {
-	for _, nombre := range nombres {
-		fmt.Println(nombre)
-	}
-}
-
-// ejemplo de closure
-// scope chaning
-var a = 0
-
-func contador() func() int {
-	c := 0
-	return func() int {
-		c++
-		return c + a
-	}
-}
-
-type Rectangulo struct {
-	Ancho, Alto float64
-}
-
-func (r Rectangulo) Area() float64 {
-	return r.Ancho * r.Alto
-}
-
 func main() {
 
-	cociente, error := dividir(10, 3)
-	if error != nil {
-		fmt.Println(error)
-		return
-	}
-	fmt.Println(cociente)
+	valor := 10
+	fmt.Println("Valor antes de incrementar:", valor)
+	incrementar(&valor) //dirección de memoria
+	fmt.Println("Valor despues de incrementar:", valor)
 
-	//Closure
-	cont := contador()
-	fmt.Println("contador: ,", cont())
-	fmt.Println("contador: ,", cont())
-	fmt.Println("contador: ,", cont())
-	fmt.Println("contador: ,", cont())
-	fmt.Println("contador: ,", cont())
-	fmt.Println("contador: ,", cont())
-
-	rect := Rectangulo{Ancho: 10, Alto: 5}
-	fmt.Println("Area: ,", rect.Area())
+	// new()
+	puntero := new(int) //puntero int inicializado en 0, donde se encuentra algo 0xc00000a0f8
+	fmt.Println("Valor inicial con new: ", puntero)
+	*puntero = 20
+	fmt.Println("Valor inicial con new: ", *puntero)
 
 }
